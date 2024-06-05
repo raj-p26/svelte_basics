00d8d7d85005a1e56956f9b5e0798f6a05ba3d3e
raj-p26 <rajpeshavariya26@gmail.com>
2024-06-06 01:09:26 +0530

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import ContactCard from "./ContactCard.svelte";
+
+	let name = "John";
+	let age = 20;
+
+	$: uppercaseName = name.toUpperCase();
+
+	$: console.log(name);
+
+	$: if (name == "Jonathan") {
+		age = 21;
+	}
+
+	let incrementAge = () => (age += 1);
+
+	/*
+	/**
+	 * @param {InputEvent} e
+	 * /
+	let changeName = (e) => {
+		name = e.target.value;
+	};
+	*/
+</script>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<!-- <button on:click={changeName}>Change Name</button> -->
+
+<!-- <input type="text" value={name} on:input={changeName} /> -->
+<!-- Shortcut for above. -->
+<input type="text" bind:value={name} />
+
+<ContactCard userName={name} />
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
