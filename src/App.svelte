478c3a0d01d45cc706e7a529b395e1709f0fe45f
raj-p26 <rajpeshavariya26@gmail.com>
2024-06-13 09:23:40 +0530

    Component lifecycle

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,74 @@
 <script>
-	import Product from "./Product.svelte";
-	import Modal from "./Modal.svelte";
+  import { tick } from "svelte";
+  import Product from "./Product.svelte";
+  import Modal from "./Modal.svelte";
 
-	/** @type {{id: string; title: string; price: number;}[]}*/
-	const products = [
-		{
-			id: "p1",
-			title: "A Book",
-			price: 9.99,
-		},
-	];
+  /** @type {{id: string; title: string; price: number;}[]}*/
+  const products = [
+    {
+      id: "p1",
+      title: "A Book",
+      price: 9.99,
+    },
+  ];
 
-	let showModal = false;
+  let showModal = false;
+  let closable = false;
+  let text = "This is some dummy text";
 
-	/** @param {CustomEvent} event */
-	const addToCart = (event) => {
-		console.log(event.detail);
-	};
+  /** @param {CustomEvent} event */
+  const addToCart = (event) => {
+    console.log(event.detail);
+  };
 
-	const delete_ = (event) => {
-		console.log(event);
-	};
+  const delete_ = (event) => {
+    console.log(event);
+  };
+
+  const transform = (event) => {
+    if (event.which !== 9) {
+      return;
+    }
+    event.preventDefault();
+
+    const selectionStart = event.target.selectionStart;
+    const selectionEnd = event.target.selectionEnd;
+    const value = event.target.value;
+
+    text =
+      value.slice(0, selectionStart) +
+      value.slice(selectionStart, selectionEnd).toUpperCase() +
+      value.slice(selectionEnd);
+
+    tick().then(() => {
+      event.target.selectionStart = selectionStart;
+      event.target.selectionEnd = selectionEnd;
+    });
+  };
 </script>
 
 {#each products as product (product.id)}
-	<Product {...product} on:add-to-cart={addToCart} on:delete={delete_} />
+  <Product {...product} on:add-to-cart={addToCart} on:delete={delete_} />
 {/each}
 
 <button on:click={() => (showModal = true)}>Show Modal</button>
 
 {#if showModal}
-	<Modal
-		on:cancel={() => (showModal = false)}
-		on:close={() => (showModal = false)}
-	>
-		<h1 slot="header">Hello!</h1>
-		<p>This works!</p>
-		<button slot="footer">Confirm</button>
-	</Modal>
+  <Modal
+    on:cancel={() => (showModal = false)}
+    on:close={() => (showModal = false)}
+    let:didAgree={closable}
+  >
+    <h1 slot="header">Hello!</h1>
+    <p>This works!</p>
+    <button
+      slot="footer"
+      disabled={!closable}
+      on:click={() => (showModal = false)}
+    >
+      Confirm
+    </button>
+  </Modal>
 {/if}
+
+<textarea rows="5" value={text} on:keydown={transform}></textarea>
--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,7 +1,35 @@
 <script>
-    import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher, onMount, onDestroy, beforeUpdate, afterUpdate } from "svelte";
 
     const dispatch = createEventDispatcher();
+
+    let agreed = false;
+    let autoscroll = false;
+
+    // When component gets mounted.
+    onMount(() => {
+        console.log('onMount');
+    });
+
+    // When component is getting destroyed.
+    onDestroy(() => {
+        console.log('onDestroy');
+    });
+
+    beforeUpdate(() => {
+        console.log('beforeUpdate');
+        autoscroll = agreed;
+    });
+
+    afterUpdate(() => {
+        console.log('afterUpdate');
+        if (autoscroll) {
+            const modal = document.querySelector('.modal');
+            modal.scrollTo(0, modal.scrollHeight);
+        }
+    });
+
+    console.log('Script executed');
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -13,9 +41,15 @@
     <div class="content">
         <slot />
     </div>
+    <div class="disclaimer">
+        <p>Before you close, you need to agree to our terms!</p>
+        <button on:click={() => (agreed = true)}>Agree</button>
+    </div>
     <footer>
-        <slot name="footer">
-            <button on:click={() => dispatch("close")}>Close</button>
+        <slot name="footer" didAgree={agreed}>
+            <button on:click={() => dispatch("close")} disabled={!agreed}
+                >Close</button
+            >
         </slot>
     </footer>
 </div>
@@ -37,7 +71,7 @@
         top: 10vh;
         left: 10%;
         width: 80%;
-        max-height: 80vh;
+        max-height: 15vh;
         background-color: white;
         border-radius: 5px;
         z-index: 100;
